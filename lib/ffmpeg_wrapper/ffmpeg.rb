caad1f9794f2b85d6524e91263c2759d7e0a564e
slowness pc <slma0x02@gmail.com>
2014-11-18 17:48:53 +0300

    begin work on ffmpeg

--- lib/ffmpeg_wrapper/ffmpeg.rb
+++ lib/ffmpeg_wrapper/ffmpeg.rb
@@ -1,5 +1,43 @@
 module FfmpegWrapper
   class FFmpeg
-    
+    class << self
+      def run(&_block)
+        @command = 'ffmpeg -y'
+        @n = 0
+        yield(self)
+        @command << ' 2>/dev/null'
+        fail `#{@command} -v error 2>&1` unless system @command
+      end
+
+      def input(filename, opts = {})
+        opts.each do |k, v|
+          @command << " -#{k} #{v}"
+        end
+        @command << " -i #{filename}"
+        @n += 1
+        @n - 1
+      end
+
+      def output(filename)
+        @command << ' ' << filename
+      end
+
+      def map(file, index = nil)
+        @command << " -map #{file}#{':' + index.to_s if index}"
+        Mapping.new @command
+      end
+    end
+  end
+
+  class Mapping < String
+    def initialize(str)
+      @cmdline = str
+    end
+
+    def applying(opts = {})
+      opts.each do |k, v|
+        @cmdline << " -#{k} #{v}"
+      end
+    end
   end
 end
--- spec/ffmpeg_spec.rb
+++ spec/ffmpeg_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper.rb'
 include FfmpegWrapper
 
 describe FFmpeg do
-  before(:all) { @ff = FFmpeg.new }
+  before(:all) { @ff = FFmpeg }
   subject { @ff }
   describe '#initialize:'
   describe '#run:' do
     after(:each) { `killall ffmpeg &>/dev/null` }
     it { should respond_to :run }
-  end
-  describe 'attributes:' do
-    it { should respond_to :inputs }
-    its(:inputs) { should be_an Array }
-    its(:outputs) { should be_a Hash }
+    it 'should make valid commands given valid input' do
+      expect do
+        FFmpeg.run do |ff|
+          v1 = ff.input 'spec/media/video.avi'
+          a1 = ff.input 'spec/media/audio.mp3'
+          ff.map(v1, 0)
+          ff.map(a1, 0).applying acodec: 'libmp3lame'
+          ff.output '~/tmp.mp4'
+        end
+      end.not_to raise_error
+    end
   end
 end
