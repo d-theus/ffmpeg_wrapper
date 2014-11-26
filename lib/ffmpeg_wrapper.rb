8bcc2bb8788577bf57053b9f767abe2138ef8def
slowness pc <slma0x02@gmail.com>
2014-11-26 13:58:39 +0300

    deleted decoder,encodec,mfile,spec

--- lib/ffmpeg_wrapper.rb
+++ lib/ffmpeg_wrapper.rb
@@ -13,11 +13,8 @@ end
 
 require 'ostruct'
 
-require 'ffmpeg_wrapper/decoder.rb'
-require 'ffmpeg_wrapper/encoder.rb'
+require 'ffmpeg_wrapper/hash.rb'
 require 'ffmpeg_wrapper/ffmpeg.rb'
 require 'ffmpeg_wrapper/format.rb'
-require 'ffmpeg_wrapper/mediafile.rb'
-require 'ffmpeg_wrapper/pixformat.rb'
 require 'ffmpeg_wrapper/stream.rb'
 require 'ffmpeg_wrapper/version.rb'
--- lib/ffmpeg_wrapper/decoder.rb
+++
@@ -1,5 +0,0 @@
-module FfmpegWrapper
-  class Decoder
-    
-  end
-end
